fdff0c76941a947c3fec676072cbac20b7d044c7
Stefan Ivanov <108619056+krav700@users.noreply.github.com>
2026-04-19 10:29:17 +0300

    feat: Form Styling

--- src/checkbox/checkbox.svelte
+++ src/checkbox/checkbox.svelte
@@ -2,7 +2,7 @@
   const { label = 'A Very Important Checkbox', id = 'checkbox', ...props } = $props();
 </script>
 
-<div>
-  <input {id} {...props} type="checkbox" />
+<div class="border bg-slate-200 p-4 rounded focus-within:bg-sky-200 outline-sky-400 outline-offset-2">
+  <input class="accent-purple-400" {id} {...props} type="checkbox" />
   <label for={id}>{label}</label>
 </div>
--- src/input/input.svelte
+++ src/input/input.svelte
@@ -4,9 +4,15 @@
   const errorId = error ? `${id}-error` : undefined;
 </script>
 
-<div>
-  <label for={id}>{label}</label>
+<div class="space-y-1">
+  <label class="block font-medium text-slate-900" for={id}>{label}</label>
   <input
+    class="block
+     w-full
+      rounded
+       bg-white
+        px-2 py-1
+         placeholder-slate-600 outline-1 placeholder:font-light user-invalid:outline-red-600 focus:outline-4 focus:outline-blue-600"
     {id}
     aria-describedby={descriptionId}
     aria-invalid={!!error}
